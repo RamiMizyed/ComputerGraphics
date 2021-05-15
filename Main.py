62e31856607520bc363a4a2fc9a7893669e30265
RamiMizyed <66327652+RamiMizyed@users.noreply.github.com>
2021-05-16 00:20:05 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,12 @@
 # imports
 import json
 from PIL import Image
+from Object3D import Object3D
+from Orthographic_Camera import OrthographicCamera
+from Hit import Hit
+from Sphere import Sphere
+from Group import Group
+from Vector3 import *
 
 # main render function takes a json file with needed information
 
@@ -14,3 +20,14 @@ def Render(filename, camera, group, background):
     resY = 500
     img = Image.new("RGB", (resX, resY))
     pixels = img.load()
+
+    for y in range(resY):
+        for x in range(resX):
+            pixels[x, y] = tuple(background)
+
+
+    for y in range(resY):
+        for x in range(resX):
+            pass
+
+
