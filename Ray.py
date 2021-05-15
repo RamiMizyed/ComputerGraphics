0ec3b029c4c9018a443d3d83d9938412a239fdbe
RamiMizyed <66327652+RamiMizyed@users.noreply.github.com>
2021-05-15 22:08:25 +0300

    Update Ray.py

--- Ray.py
+++ Ray.py
@@ -1,8 +1,7 @@
 import Vector3
 
+
 class Ray:
-    def __init__(self, origin:Vector3, direction:Vector3):
+    def __init__(self, origin: Vector3, direction: Vector3):
         self.origin = origin
         self.direction = direction
-
-
